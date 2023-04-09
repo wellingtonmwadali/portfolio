3d16fac30c7123b9523d401d53d103582590b295
Mwadali <wellingtonmwadali@gmail.com>
2023-04-09 22:58:48 +0300

    Added profiles to response js file

--- assets/response.js
+++ assets/response.js
@@ -3,13 +3,35 @@ function getBotResponse(input){
     if(input == "hello"){
         return "Hey there!"
     }else if(input == "more") {
-        return "Hi,my abilities are limited to the following prompts :\n-Reveal yourself.\n-Navigation.\n-Profiles.\n-Humour.\n-Kwaheri"
+        return "Hi,my abilities are limited to the following prompts :\n-Reveal yourself.\n-Navigation.\n-Game.\n-Profiles.\n-Humour.\n-Kwaheri"
     }
     else if(input == "Reveal yourself"){
-        "My name is Chao.I identify myself as a feminine character.My purpose is to act as your guide within my abilities."
+        "My name is Chao.I identify myself as a feminine character.My purpose is to act as your guide within my abilities.Use options as prompts for more info "
     }
     else if(input == "Navigation"){
-        return "page one or page two?"
+        return "page one or page two ?"
+    }
+    else if(input == "page one"){
+        return "Input the hint '1010101' to view the page from a different angle.Welcome to the world of animations."
+    }
+    else if(input == "page two"){
+        return "The page is divided into different sections.To view the complete site resize the width of your screen then view in complete window the game or projects file."
+    }
+    else if(input == "Game"){
+        return "The rules for the game as stated is you pick a range numbers,feel free to go wild with numbers.The range of numbers will determine the number of attempts.If you exhaust your attempts you will be prompted to try again.GOOD LUCKYou can try your luck with my game.Click index after you are done."
+    }
+    else if (input == "Profiles"){
+        return "github, linkedin or twitter? "
+    }
+    else if (input == "github" || "Github"){
+        return "https://github.com/wellingtonmwadali"
+    }
+    else if (input == "linkedin" || "Linkedin"){
+        return "https://www.linkedin.com/in/wellington-mwadali-/"
+    }
+    else if (input == "twitter" || "Twitter"){
+        return "https://twitter.com/mwadzaliii"
     }
     
+
 }
