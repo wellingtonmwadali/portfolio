9dd1fd254941c76c197e57a37c8af47339f9b633
Mwadali <wellingtonmwadali@gmail.com>
2023-04-10 00:04:26 +0300

    updated profile links

--- assets/response.js
+++ assets/response.js
@@ -18,25 +18,25 @@ function getBotResponse(input){
         return "The page is divided into different sections.To view the complete site resize the width of your screen then view in complete window the game or projects file."
     }
     else if(input == "Game"){
-        return "The rules for the game as stated is you pick a range numbers,feel free to go wild with numbers.The range of numbers will determine the number of attempts.If you exhaust your attempts you will be prompted to try again.GOOD LUCKYou can try your luck with my game.Click index after you are done."
+        return "The rules for the game as stated is you pick a range numbers,feel free to go wild with numbers.The range of numbers will determine the number of attempts.If you exhaust your attempts you will be prompted to try again.GOOD LUCK.Click index after you are done."
     }
     else if (input == "Profiles"){
         return "github, linkedin or twitter? "
     }
     else if (input == "github"){
-        return "https://github.com/wellingtonmwadali"
+        return '<a href="https://github.com/wellingtonmwadali">https://github.com/wellingtonmwadali</a>'
     }
     else if (input == "linkedin"){
-        return "https://www.linkedin.com/in/wellington-mwadali-/"
+        return '<a href="https://www.linkedin.com/in/wellington-mwadali-/">https://www.linkedin.com/in/wellington-mwadali-'
     }
     else if (input == "twitter"){
-        return "https://twitter.com/mwadzaliii"
+        return '<a href ="https://twitter.com/mwadzaliii">https://twitter.com/mwadzaliii</a>'
     }
     else if (input == "Humour"){
         return "dry, dark or intelligent?"
     }
     else if(input == "dry"){
-        return "Doctor at my check up asked me if I had 'any feelings of depression or anxiety'and I said 'don't we all'and she said 'no'"
+        return "Doctor at my check up asked me if I had 'any feelings of depression or anxiety'and I said 'don't we all'and she said 'no'.lol"
     }
     else if(input == "dark"){
         return "Want to turn your depression to profit,wake up at noon so you can only pay for two meals a day "
